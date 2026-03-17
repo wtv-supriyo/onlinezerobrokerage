bb417e6424093bdaeac3bedfc0015aeed22cbd54
wtv-vijay <vijay.wtv@gmail.com>
2026-03-17 16:51:51 +0530

    next and react version problem fix

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
