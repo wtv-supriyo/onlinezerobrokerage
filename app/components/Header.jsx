5d3ce9f9822ec7195939004a554a975fc7bdbfdb
Supriyo <114462230+wtv-supriyo@users.noreply.github.com>
2026-06-25 20:38:22 +0530

    header name change

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
           >
             <path d="M3 12l2-2 4 4 8-8 2 2-10 10-6-6z" />
           </svg>
-          <span>BestStockbrokerIndia</span>
+          <span>onlinezerobrokerage</span>
         </Link>
 
         {/* Desktop Nav */}
